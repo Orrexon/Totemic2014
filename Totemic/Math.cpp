1f1d3d64344c00d97ad11322d69c87ff3b05d9be
Orrexon <orrexon@gmail.com>
2014-05-14 23:54:09 +0200

    some functions in Math

--- Totemic/Math.cpp
+++ Totemic/Math.cpp
@@ -56,5 +56,27 @@ namespace Math
 		float length = vectorLength(delta);
 		return sf::Vector2f(delta.x / length, delta.y / length);
 	}
+
+	float Math::sumMagnitude(sf::Vector2f& V1, sf::Vector2f& V2)//overloaded for flexibility, just use any type of vector
+	{
+		return sqrtf(V1.x*V1.x + V1.y*V1.y) + sqrtf(V2.x*V2.x + V2.y*V2.y);
+	}
+
+	float Math::sumMagnitude(const b2Vec2& V1, const b2Vec2& V2)
+	{
+		return V1.Length() + V2.Length();
+	}
+
+	float Math::sumMagnitude(b2Vec2& V1, sf::Vector2f& V2)
+	{
+		return V1.Length() + sqrtf(V2.x * V2.x + V2.y * V2.y);
+	}
+
+	float Math::sumMagnitude(sf::Vector2f& V1, b2Vec2& V2)
+	{
+		return sqrtf(V1.x*V1.x + V1.y*V1.y) + V2.Length();
+	}
 }
+
+
 
